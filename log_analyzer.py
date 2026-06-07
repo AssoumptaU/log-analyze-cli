96ac0e91b71c437bd49b5f5538f7d3a96f952155
Assoumpta  Uwabera <assoumpta.uwabera@testsolutions.de>
2026-06-07 23:43:18 +0200

    Completed Feature-2: Detect log format

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,19 +1,28 @@
-# We need to tell Python the name of the file we want to open
+import json
+
 file_to_open = "app.log"
 
-print("Starting the Log Analyzer ...")
+print("--- Starting the Log Analyzer ---")
 
-# 'with open' tells Python to unlock and open the file. 
-# 'r' means we only want to 'read' the file (not change it).
 with open(file_to_open, "r") as file:
-    
-    # This loop tells Python: "Go through the file, one single line at a time"
     for line in file:
-        
-        # .strip() removes the invisible 'Enter' key spaces at the end of each line
         clean_line = line.strip()
         
-        # Print the line to our terminal screen
-        print(clean_line)
+        # --- FEATURE 2: DETECT FORMAT ---
+        try:
+            # Parse the line into a dictionary
+            structured_json_data = json.loads(clean_line)
+            
+            # Extract all THREE required fields 
+            log_timestamp = structured_json_data['timestamp']
+            log_level = structured_json_data['level']
+            log_message = structured_json_data['message']
+            
+            # Print all three extracted fields clearly
+            print(f"Detected JSON Format -> Timestamp: {log_timestamp}, Level: {log_level}, Message: {log_message}")
+            
+        except json.JSONDecodeError:
+            # If it's not JSON, it's a plain text line
+            print(f"Detected Plain Text Format -> {clean_line}")
 
-print("Finished reading the file!")
+print("--- Finished reading the file ---")
